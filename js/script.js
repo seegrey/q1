b4c13bb6fc365f9265cdd56aa9305057e482ccb3
seegrey <seegrey@g.c>
2022-01-17 18:27:25 +0200

    last

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 "use strict";
 
+let numberOfFilms;
 
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+personalMovieDB.movies[0] = prompt("ваш любимый фильм, '");
+personalMovieDB.movies[1] = prompt("оценка фильма", "");
+
+
+console.log(personalMovieDB.movies);
